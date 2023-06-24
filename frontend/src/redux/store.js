23cc3dde5da8f141db6b4d4c6ff7fbd33ee4ebf8
Abinash <abinashmohanty227@gmail.com>
2023-06-24 20:18:21 +0530

    2nd

--- frontend/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import productSlideReducer from "./productSlide";
-export const store=configureStore({
-    reducer:{
-        product:productSlideReducer
-    },
-})
